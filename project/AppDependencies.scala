83498c857f0ba78763c61b376e116c5934abcfb1
RobertLaverick <10117445+RobertLaverick@users.noreply.github.com>
2024-08-19 11:43:03 +0100

    updated frontend

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "10.8.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "10.9.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "io.lemonlabs"            %% "scala-uri"                  % "4.0.3"
   )
